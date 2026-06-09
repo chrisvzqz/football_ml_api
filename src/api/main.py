ac2e594f4e33890b9fbc56a5352f0697f4ded67c
Christian <chris@mbpdechristian.home>
2026-06-09 09:11:11 +0200

    feat: add basic logging

--- src/api/main.py
+++ src/api/main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI
 import pandas as pd
 from src.inference.predictor import MatchPredictor
 from src.api.schemas import MatchRequest
+import logging
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 app = FastAPI()
+
 predictor = MatchPredictor()
+logger.info("Model loaded successfully.")
 
 @app.get("/health")
 def health_check():
@@ -13,5 +18,15 @@ def health_check():
 @app.post("/predict")
 def predict(match: MatchRequest):
     match_df = pd.DataFrame(match.model_dump(), index=[0])
-    prediction = predictor.predict(match_df)
-    return {"predicted_winner": prediction}
+    logger.info(
+        f"Prediction requested: {match.home_team} vs {match.away_team}"
+    )
+
+    try:
+        prediction = predictor.predict(match_df)
+        logger.info(f"Prediction result: {prediction}")
+        return {"predicted_winner": prediction}
+
+    except Exception as e:
+        logger.error(f"Prediction failed: {e}")
+        raise # Lanza la misma excepción que acaba de capturar
